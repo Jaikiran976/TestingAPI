905b8b0277a8d4c99ec0a1f02b0792f779cbf3a1
Jaikiran976 <142710462+Jaikiran976@users.noreply.github.com>
2025-04-23 11:16:18 +0530

    New Code
    Pushing the code

---
+++ Frontend/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { WeatherForecast } from './Models/weather.module';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'Frontend';
+  private http = inject(HttpClient);
+
+  weatherlist:any;
+
+  constructor(){
+    this.getList().subscribe({
+      next:(params)=>{
+        this.weatherlist = params;
+      }
+    })
+  }
+
+  getList():Observable<WeatherForecast[]>{
+    return this.http.get<WeatherForecast[]>('https://localhost:7259/WeatherForecast');
+  }
+}
