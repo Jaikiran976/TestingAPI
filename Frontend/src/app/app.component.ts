70752f9896bc7979d1657ce20b12b4400f69d50b
Jaikiran976 <142710462+Jaikiran976@users.noreply.github.com>
2025-04-25 12:00:20 +0530

    adding new changes

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+import { WeatherForecast } from './Models/weather.module';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +12,19 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'FrontEnd';
+  private http = inject(HttpClient);
+
+  weatherlist:any;
+
+  constructor(){
+    this.getList().subscribe({
+      next:(params: any)=>{
+        this.weatherlist = params;
+      }
+    })
+  }
+
+  getList():Observable<WeatherForecast[]>{
+    return this.http.get<WeatherForecast[]>('https://testingapi-akm0.onrender.com/WeatherForecast');
+  }
 }
